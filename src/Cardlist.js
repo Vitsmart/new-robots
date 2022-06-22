340db7477c493841dd0a825543dfdae2bdc41767
Igboanugwo Collins <munisco12@gmail.com>
2022-06-22 14:26:29 +0100

    added src folder

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card1 from './Card1';
 
 const  Cardlist = ({ robots1 }) => {
- Card1component = robots1.map((user, i) => {
+ const Card1component = robots1.map((user, i) => {
     return (
          <Card1 
     id={robots1[i].id} 
