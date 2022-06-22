fd6c967d5fc5884facf2b24cf4b32c465416750f
Igboanugwo Collins <munisco12@gmail.com>
2022-06-22 14:38:45 +0100

    restructed files and fixed error by initialising the variable Card1component

--- src/Cardlist.js
+++ src/Cardlist.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card1 from './Card1';
 
 const  Cardlist = ({ robots1 }) => {
+    //I added "const" to the variable Card1component, you can check your file for changes, i also changed robots1[i] to user, it doesn't actually make a difference, just for readability... P.S :Good work by the way, more grace
  const Card1component = robots1.map((user, i) => {
     return (
          <Card1 
